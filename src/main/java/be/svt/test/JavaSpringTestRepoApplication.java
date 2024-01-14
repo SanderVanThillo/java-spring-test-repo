a7709b40f55329513a01d547b3e8d42a6bf5b45e
Sander Van Thillo <sandervanthillo@gmail.com>
2024-01-14 11:27:19 +0100

    setup spring and gradle in project

---
+++ src/main/java/be/svt/test/JavaSpringTestRepoApplication.java
@@ -0,0 +1,13 @@
+package be.svt.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTestRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringTestRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/svt/test/JavaSpringTestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package be.svt.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTestRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
